3855e3284cfc45a2cf371e250bc677b3ad56ba49
sayklang <sayklang.sar@student.mahidol.edu>
2025-02-24 08:36:27 +0000

    dhgb

--- main.c
+++ main.c
@@ -14,16 +14,16 @@ for ( i = 1; i < argc; ++i ) {
    //  insertNode_R(&b.root,atoi(argv[i]) );
   } // end for
    // traverse the tree preOrder
-   /* printf("\nThe preOrder traversal is:\n" );
-   preOrder( rootPtr );*/
+   printf("\nThe preOrder traversal is:\n" );
+   preOrder( b.root );
 
    // traverse the tree inOrder
    printf("\nThe inOrder traversal is:\n" );
    inOrder( b.root );
-/*
+
    // traverse the tree postOrder
- printf("\nThe postnOrder traversal is:\n" );   postOrder( rootPtr );
-*/
+ printf("\nThe postnOrder traversal is:\n" );   postOrder(b.root );
+
     printf("\n");
 } // end main
 
